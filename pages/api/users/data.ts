a3a2d752d17c420b226f2847469c8984a6a28ec5
Rushil Gupta <rushil.rushil.gupta@gmail.com>
2022-05-18 20:05:28 +0530

    updates votes api

--- pages/api/users/data.ts
+++ pages/api/users/data.ts
@@ -1,19 +1,48 @@
 interface UserInterface {
     emp_id: number;
     name: string;
+    profile_pic: string;
 }
 
 export const Users: UserInterface[] = [
     {
         emp_id: 10001,
         name: "John Doe",
+        profile_pic: "https://randomuser.me/api/portraits/men/83.jpg",
     },
     {
         emp_id: 10002,
-        name: "Franklin",
+        name: "Alicia",
+        profile_pic: "https://randomuser.me/api/portraits/women/60.jpg",
     },
     {
         emp_id: 10003,
         name: "Lincon",
+        profile_pic: "https://randomuser.me/api/portraits/men/84.jpg",
+    },
+    {
+        emp_id: 10004,
+        name: "Hermione",
+        profile_pic: "https://randomuser.me/api/portraits/women/56.jpg",
+    },
+    {
+        emp_id: 10005,
+        name: "Draco",
+        profile_pic: "https://randomuser.me/api/portraits/men/85.jpg",
+    },
+    {
+        emp_id: 10006,
+        name: "Ginny",
+        profile_pic: "https://randomuser.me/api/portraits/women/57.jpg",
+    },
+    {
+        emp_id: 10007,
+        name: "James",
+        profile_pic: "https://randomuser.me/api/portraits/men/87.jpg",
+    },
+    {
+        emp_id: 10008,
+        name: "James",
+        profile_pic: "https://randomuser.me/api/portraits/men/88.jpg",
     },
 ];
